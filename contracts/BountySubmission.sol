1d9251996e9d6dfad0c73a87a7c2d70f55700161
Pedro R. Jiménez <ptpedroj@gmail.com>
2019-01-28 06:20:46 -0500

    Made BountySubmission Pausable

--- contracts/BountySubmission.sol
+++ contracts/BountySubmission.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.5.0;
 import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
 import "openzeppelin-solidity/contracts/lifecycle/Pausable.sol";
 
-contract BountySubmission is Ownable  {
+contract BountySubmission is Ownable, Pausable  {
     string internal submission;
 
     event LogSubmissionSet(string newSubmission);
@@ -12,7 +12,7 @@ contract BountySubmission is Ownable  {
         submission = newSubmission;
     }
 
-    function setSubmission(string memory newSubmission) public onlyOwner {
+    function setSubmission(string memory newSubmission) public whenNotPaused onlyOwner {
         submission = newSubmission;
         emit LogSubmissionSet(newSubmission);
     }
